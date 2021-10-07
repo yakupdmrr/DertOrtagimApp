d5bbe034b40a1d05b824bdc9e58345e9c45d7788
yakupdmrr <yakupdmrr@hotmail.com>
2021-10-07 22:43:12 +0300

    Initial Commit

---
+++ DertOrtagim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DertOrtagim
+//
+//  Created by Yakup Demir on 7.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
